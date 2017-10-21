0fbf504a5f1894be6d70c7ab1560ff4c60f21e17
Brantley Vose <brantmv@iastate.edu>
2017-10-21 00:47:56 -0500

    First commit with Arduino sketches.

---
+++ Arduino Sketches/PS2Test/PS2Test.ino
@@ -0,0 +1,23 @@
+//Actually PS1
+
+#include <Psx.h>
+
+const int dataPin = 9;
+const int cmndPin = 12;
+const int attPin = 11;
+const int clockPin = 10;
+
+const int LEDPin = 13;
+
+Psx controller;
+void setup() {
+  pinMode(LEDPin, OUTPUT);
+  controller.setupPins(dataPin,cmndPin,attPin,clockPin,10);
+  Serial.begin(9600);
+}
+int data = 0;
+void loop() {
+  data = controller.read();
+  digitalWrite(LEDPin, (data&psxO)? HIGH:LOW);
+  delay(1);
+}
---
+++ Arduino Sketches/PS2Test2/PS2Test2.ino
@@ -0,0 +1,11 @@
+//The last one was for Ps1. This one actually is for Ps2.
+//Pretty sure it'll work just fine.
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
