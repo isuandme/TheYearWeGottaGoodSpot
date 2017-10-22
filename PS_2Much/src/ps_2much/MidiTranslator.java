c78a5a51cbb05a4d63ada5d0bebf9beb46493974
Brantley Vose <brantmv@iastate.edu>
2017-10-22 06:00:20 -0500

    Small fixes.

--- PS_2Much/src/ps_2much/MidiTranslator.java
+++ PS_2Much/src/ps_2much/MidiTranslator.java
@@ -6,20 +6,20 @@ public class MidiTranslator extends TeVirtualMIDI{
 	public MidiTranslator(String s){
 		super(s);
 	}
-	public static void main(String[] args) throws InterruptedException {
-		MidiTranslator test = new MidiTranslator("Test3");
-		//byte[] command = {(byte)144,60,100};
-		byte j = 0;
-		while(true){
-			for(int i = LOWBUTTON; i < HIGHBUTTON; i++){
-				j++;
-				test.translate((byte) 16, j);
-				//test.translate((byte)i,(byte)1);
-				Thread.sleep(25);
-				//test.translate((byte)i, (byte)0);
-			}
-		}
-	}
+//	public static void main(String[] args) throws InterruptedException {
+//		MidiTranslator test = new MidiTranslator("Test3");
+//		//byte[] command = {(byte)144,60,100};
+//		byte j = 0;
+//		while(true){
+//			for(int i = LOWBUTTON; i < HIGHBUTTON; i++){
+//				j++;
+//				test.translate((byte) 16, j);
+//				//test.translate((byte)i,(byte)1);
+//				Thread.sleep(25);
+//				//test.translate((byte)i, (byte)0);
+//			}
+//		}
+//	}
 	
 	public boolean translate(byte first, byte second){
 
