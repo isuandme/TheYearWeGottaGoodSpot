92da60cecb8c78ba76c28232ae3aec32ef06b8f1
Brantley Vose <brantmv@iastate.edu>
2017-10-21 20:30:02 -0500

    Import necessary libraries for MIDI.

--- PS_2Much/src/ps_2much/MidiTranslator.java
+++ PS_2Much/src/ps_2much/MidiTranslator.java
@@ -1,5 +1,11 @@
 package ps_2much;
-
+import de.tobiaserichsen.*;
+import de.tobiaserichsen.tevm.TeVirtualMIDI;
 public class MidiTranslator {
-
+	static TeVirtualMIDI virtual;
+	public static void main(String[] args) throws InterruptedException {
+		virtual = new TeVirtualMIDI("Test");
+		Thread.sleep(5000);
+		virtual.shutdown();
+	}
 }
